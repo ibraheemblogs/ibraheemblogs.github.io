1773cbd092340bc7465776d72b930e6b1e08e442
jafarOlamide <jafarolamidekale@gmail.com>
2025-02-09 23:51:09 +0100

    redeploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "ibraheemblogs",
+  base: "https://sharialabs.com/",
   plugins: [react(), tailwindcss(),],
 })
